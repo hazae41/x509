b5879795da498a6c897c8582b74ed07ed41a0a9b
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2022-12-04 18:46:35 +0100

    dist

--- dist/types/mods/asn1/der.d.ts
+++
@@ -1,8 +0,0 @@
-import { Binary } from '../../libs/binary/binary.js';
-import { ToStringable } from './types.js';
-
-declare namespace DER {
-    function parse(binary: Binary): ToStringable;
-}
-
-export { DER };
--- dist/types/mods/x509/pem/pem.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace PEM {
-    const header = "-----BEGIN CERTIFICATE-----";
-    const footer = "-----END CERTIFICATE-----";
-    function parse(text: string): Buffer;
-    function stringify(buffer: Buffer): string;
-}
-
-export { PEM };
