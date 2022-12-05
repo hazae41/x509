879dafd23369fd48e9d6d7879223dfda2668790e
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2022-12-05 13:18:39 +0100

    clean

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,1 @@
-export * from "libs/binary/binary.test.js";
-export * from "libs/bitset/bitset.test.js";
 export * from "mods/index.test.js";
--- src/libs/buffers/buffers.ts
+++
@@ -1,8 +0,0 @@
-export namespace Buffers {
-
-  export function toBinary(buffer: Buffer) {
-    const bn = BigInt("0x" + buffer.toString("hex"))
-    return bn.toString(2).padStart(buffer.length * 8, "0")
-  }
-
-}
