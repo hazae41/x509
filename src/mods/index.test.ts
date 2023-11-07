e767e02f7969e63e6f62fce8463262d741292db4
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-11-07 18:15:59 +0100

    clean

--- src/mods/index.test.ts
+++ src/mods/index.test.ts
@@ -8,7 +8,7 @@ import { readFile } from "fs/promises";
 import { PEM } from "mods/pem/pem.js";
 import { Certificate } from "mods/types/certificate/certificate.js";
 import { relative, resolve } from "path";
-import { readFromBytesOrThrow, tryWriteToBytes } from "./types/types.js";
+import { readFromBytesOrThrow, writeToBytesOrThrow } from "./types/index.js";
 
 const directory = resolve("./dist/test/")
 const { pathname } = new URL(import.meta.url)
@@ -20,46 +20,46 @@ await test("Cert Ed25519", async () => {
   const bytes = PEM.tryDecode(await readFile("./certs/ed25519.pem", "utf8")).unwrap()
   const cert = readFromBytesOrThrow(Certificate, bytes)
 
-  assert(Bytes.equals(bytes, tryWriteToBytes(cert).unwrap()))
+  assert(Bytes.equals(bytes, writeToBytesOrThrow(cert)))
 })
 
 await test("Cert Let's Encrypt", async () => {
   const bytes = PEM.tryDecode(await readFile("./certs/letsencrypt.pem", "utf8")).unwrap()
   const cert = readFromBytesOrThrow(Certificate, bytes)
 
-  assert(Bytes.equals(bytes, tryWriteToBytes(cert).unwrap()))
+  assert(Bytes.equals(bytes, writeToBytesOrThrow(cert)))
 })
 
 await test("Cert frank4dd-rsa", async () => {
   const bytes = await readFile("./certs/frank4dd-rsa.der")
   const cert = readFromBytesOrThrow(Certificate, bytes)
 
-  assert(Bytes.equals(bytes, tryWriteToBytes(cert).unwrap()))
+  assert(Bytes.equals(bytes, writeToBytesOrThrow(cert)))
 })
 
 await test("Cert frank4dd-dsa", async () => {
   const bytes = await readFile("./certs/frank4dd-dsa.der")
   const cert = readFromBytesOrThrow(Certificate, bytes)
 
-  assert(Bytes.equals(bytes, tryWriteToBytes(cert).unwrap()))
+  assert(Bytes.equals(bytes, writeToBytesOrThrow(cert)))
 })
 
 await test("Cert Tor", async () => {
   const bytes = PEM.tryDecode(await readFile("./certs/tor.pem", "utf8")).unwrap()
   const cert = readFromBytesOrThrow(Certificate, bytes)
 
-  assert(Bytes.equals(bytes, tryWriteToBytes(cert).unwrap()))
+  assert(Bytes.equals(bytes, writeToBytesOrThrow(cert)))
 })
 
 test("Cert Tor 2", async () => {
   const bytes = PEM.tryDecode(await readFile("./certs/tor2.pem", "utf8")).unwrap()
   const cert = readFromBytesOrThrow(Certificate, bytes)
 
-  assert(Bytes.equals(bytes, tryWriteToBytes(cert).unwrap()))
+  assert(Bytes.equals(bytes, writeToBytesOrThrow(cert)))
 })
 
 test("Cert full", async () => {
   const bytes = PEM.tryDecode(await readFile("./certs/full.pem", "utf8")).unwrap()
   const cert = readFromBytesOrThrow(Certificate, bytes)
-  assert(Bytes.equals(bytes, tryWriteToBytes(cert).unwrap()))
+  assert(Bytes.equals(bytes, writeToBytesOrThrow(cert)))
 })
--- src/mods/types/index.ts
+++ src/mods/types/index.ts
@@ -10,5 +10,60 @@ export * from "./relative_distinguished_name/relative_distinguished_name.js";
 export * from "./subject_public_key_info/subject_public_key_info.js";
 export * from "./tbs_certificate/tbs_certificate.js";
 export * from "./tbs_certificate/tbs_certificate_version.js";
-export * from "./types.js";
 export * from "./validity/validity.js";
+
+import { DER, DERCursor, DERable } from "@hazae41/asn1";
+import { ReadError, Readable, Writable, WriteError } from "@hazae41/binary";
+import { Result } from "@hazae41/result";
+
+export interface Resolvable<T> {
+  resolveOrThrow(cursor: DERCursor): T
+}
+
+export class ResolveError extends Error {
+  readonly #class = ResolveError
+  readonly name = this.#class.name
+
+  constructor(options: ErrorOptions) {
+    super(`Could not resolve`, options)
+  }
+
+  static from(cause: unknown) {
+    return new ResolveError({ cause })
+  }
+
+}
+
+export function resolveOrThrow<T>(resolvable: Resolvable<T>, cursor: DERCursor): T {
+  return resolvable.resolveOrThrow(cursor)
+}
+
+export function tryResolve<T>(resolvable: Resolvable<T>, cursor: DERCursor): Result<T, ResolveError> {
+  return Result.runAndWrapSync(() => {
+    return resolvable.resolveOrThrow(cursor)
+  }).mapErrSync(ResolveError.from)
+}
+
+export function readFromBytesOrThrow<T>(resolvable: Resolvable<T>, bytes: Uint8Array): T {
+  const triplet = Readable.readFromBytesOrThrow(DER, bytes)
+  const cursor = new DERCursor([triplet])
+
+  return resolvable.resolveOrThrow(cursor)
+}
+
+export function tryReadFromBytes<T>(resolvable: Resolvable<T>, bytes: Uint8Array): Result<T, ReadError | ResolveError> {
+  return Result.unthrowSync(t => {
+    const triplet = Readable.tryReadFromBytes(DER, bytes).throw(t)
+    const cursor = new DERCursor([triplet])
+
+    return tryResolve(resolvable, cursor)
+  })
+}
+
+export function writeToBytesOrThrow(type: DERable<Writable>): Uint8Array {
+  return Writable.writeToBytesOrThrow(type.toDER())
+}
+
+export function tryWriteToBytes(type: DERable<Writable>): Result<Uint8Array, WriteError> {
+  return Writable.tryWriteToBytes(type.toDER())
+}
